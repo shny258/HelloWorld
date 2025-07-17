51f29536a5e62d236b65ad2ad4ee9fa84d559762
shny258 <69259428+shny258@users.noreply.github.com>
2025-07-17 11:56:51 -0300

    Create HelloWorld

---
+++ HelloWorld.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main() {
+    vector<string> msg {"Hello","C++", "World", "from", "VS Code", "and the C++ extension!"};
+    for (const auto& word : msg) {
+        cout << word << " ";
+    }
+    cout << endl;
+    return 0;   
+}
